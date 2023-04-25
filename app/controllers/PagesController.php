e351bac0d5a87165d2a9ec5343390859e8d778af
matteo <matteo.razzanelli89@gmail.com>
2023-04-25 19:25:10 +0200

    refactor

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -17,32 +17,6 @@ class PagesController
    */
   public function home()
   {
-    // $users = App::get('database')->selectAll('users', 'User');
-    // $content = $_REQUEST['content'];
-    // // require 'views/index.view.php';
-    // return view('index', [
-    //   'users' => $users,
-    //   'content' => $content
-    // ]);
     view('index');
-  }
-
-  /**
-   * Show the about page.
-   */
-  public function about()
-  {
-    // require 'views/about.view.php';
-    $company = 'Laracasts';
-    view('about', ['company' => $company]);
-  }
-
-  /**
-   * Show the contact page.
-   */
-  public function contact()
-  {
-    // require 'views/contact.view.php';
-    view('contact');
   }
 }
