42830590124047c4495fcfc1dddcc9613e75bede
matteo <matteo.razzanelli89@gmail.com>
2023-04-26 08:03:40 +0200

    minor

--- core/helpers.php
+++ core/helpers.php
@@ -1,23 +1,12 @@
 <?php
 
-/**
- * Require a view.
- *
- * @param  string $name
- * @param  array  $data
- */
-function view($name, $data = [])
+// TODO: use a dedicated class for response
+function view($pageName, $data = [])
 {
     extract($data);
-    // http_response_code(404);
-    return require "app/views/{$name}.view.php";
+    return require "app/views/{$pageName}.view.php";
 }
 
-/**
- * Redirect to a new page.
- *
- * @param  string $path
- */
 function redirect($path)
 {
     header("Location: /{$path}");
