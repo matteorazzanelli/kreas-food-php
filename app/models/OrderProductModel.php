9cdf10438fd93fda84466ed660b8c10c1a26a5df
matteo <matteo.razzanelli89@gmail.com>
2023-05-14 19:01:27 +0200

    refactor: modify the query for the custom model to filter results

--- app/models/OrderProductModel.php
+++ app/models/OrderProductModel.php
@@ -36,14 +36,42 @@ class OrderProductModel extends Model{
     return $this->storeOrderProduct($idOrder, $products, $quantities, $model);
   }
 
-  public function sumCO2ByProduct($idProduct){
-    $query = "SELECT SUM(quantity) as sum from orders_products as op 
-    INNER JOIN products as p ON op.id_product=p.id 
-    WHERE p.id=$idProduct;";
+  public function sumCO2ByProperty($data){
+    extract($data);
+    $query = 
+      "SELECT p.id,SUM(quantity) as sum from orders_products as op 
+      INNER JOIN products as p ON op.id_product=p.id
+      INNER JOIN orders as o on op.id_order=o.id";
+    
+    // TODO: handle better the combinations
+    if($product || $country)
+      $query = $query . ' WHERE';
+    if($product)
+      $query = $query . ' p.id=:product_prop';
+    if($product && $country)
+      $query = $query . ' AND';
+    if($country)
+      $query = $query . ' o.id=:country_prop';
+
+    if($fromDate && !($product || $country))
+      $query = $query . ' WHERE o.date BETWEEN :from_date AND :to_date';
+    if($fromDate && ($product || $country))
+      $query = $query . ' AND o.date BETWEEN :from_date AND :to_date';
+
+    $query = $query . " GROUP BY p.id;";
 
     $st = $this->pdo->prepare($query);
+    if($product)
+      $st->bindValue(":product_prop", $product);
+    if($country)
+      $st->bindValue(":country_prop", $country);
+    if($fromDate){
+      $st->bindValue(":from_date", $fromDate);
+      $st->bindValue(":to_date", $toDate);
+    }
     $st->execute();
 
-    return $st->fetch(\PDO::FETCH_ASSOC);
+    return $st->fetchAll(\PDO::FETCH_ASSOC);
   }
 }
+
