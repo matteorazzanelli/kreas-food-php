15cdadd4c41b7412277f3ff36383875577c97666
matteo <matteo.razzanelli89@gmail.com>
2023-05-31 07:35:24 +0200

    feat: add a new specific function for filtering by property

--- app/controllers/StatisticsController.php
+++ app/controllers/StatisticsController.php
@@ -18,7 +18,6 @@ class StatisticsController extends Controller
         $model = App::get('model');
         $order_product = new OrderProductModel($model->getPDO());
         $result = $order_product->sumCO2Total();
-        // die(var_dump($result));
 
         if(!$result) {
           $this->setCode(400);
@@ -35,33 +34,43 @@ class StatisticsController extends Controller
           'page' => 'statistics',
           'message' => 'OK'
         ]);
-
-    }
-        $products_quantities = $order_product->sumCO2ByProperty([
-          'product' =>  $_GET['name'] ? $products['id'] : false,
-          'country' => $_GET['country'] ? $orders['id'] : false,
-          'fromDate' => $_GET['start_date'] ?? false,
-          'toDate' => $_GET['end_date'] ?? false
-        ]);
-
-        $sumProducts = 0;
-
-        foreach($products_quantities as $p_q) {
-            $p = $model->getElementFromProperty('products', 'id', $p_q['id']);
-            $sumProducts += intval($p_q['sum'])*intval($p['co2']);
-        }
-
-        $this->setCode(200);
-        return $this->renderApi([
-          'result' => $sumProducts,
-          'page' => 'statistics',
-          'message' => 'OK'
-        ]);
-
     }
 
     public function filter()
     {
+      $model = App::get('model');
 
+      // Validate inputs: should exist a specific class to handle them
+      $products = $_GET['name'] ?
+        $model->getElementFromProperty('products', 'name', $_GET['name']) :
+        $model->selectAll('products', 'App\\Models\\ProductModel');
+
+      $orders = $_GET['country'] ?
+        $model->getElementFromProperty('orders', 'country', $_GET['country']) :
+        $model->selectAll('orders', 'App\\Models\\OrderModel');
+
+      if(!$products || !$orders) {
+        $this->setCode(400);
+        return $this->renderApi([
+          'result' => 0,
+          'page' => 'filter',
+          'message' => 'bad request'
+        ]);
+      }
+
+      $order_product = new OrderProductModel($model->getPDO());
+      $result = $order_product->sumCO2ByProperty([
+        'product' =>  $_GET['name'] ? $products['id'] : false,
+        'country' => $_GET['country'] ? $orders['id'] : false,
+        'fromDate' => $_GET['start_date'] ?? false,
+        'toDate' => $_GET['end_date'] ?? false
+      ]);
+
+      $this->setCode(200);
+      return $this->renderApi([
+        'result' => $result['total'],
+        'page' => 'filter',
+        'message' => 'OK'
+      ]);
     }
 }
--- app/models/OrderProductModel.php
+++ app/models/OrderProductModel.php
@@ -39,7 +39,8 @@ class OrderProductModel extends Model
         return $this->storeOrderProduct($idOrder, $products, $quantities, $model);
     }
 
-    public function sumCO2Total(){
+    public function sumCO2Total()
+    {
         $query =
           "SELECT SUM(t.sum*p.co2) as total from 
           (
@@ -58,15 +59,32 @@ class OrderProductModel extends Model
     public function sumCO2ByProperty($data)
     {
         extract($data);
+
+        // if data not set, set them to min/today
+        if(!$fromDate) {
+            $query = "SELECT CAST('1753-1-1' as DATE) as fromDate;";
+            $st = $this->pdo->prepare($query);
+            $st->execute();
+            $res = $st->fetch(\PDO::FETCH_ASSOC);
+            $fromDate = $res['fromDate'];
+        }
+
+        if(!$toDate){
+            $query = "SELECT CAST(CURRENT_TIMESTAMP as DATE) as toDate;";
+            $st = $this->pdo->prepare($query);
+            $st->execute();
+            $res = $st->fetch(\PDO::FETCH_ASSOC);
+            $toDate = $res['toDate'];
+        }
+        
         $query =
-          "SELECT p.id,SUM(quantity) as sum from orders_products as op 
-      INNER JOIN products as p ON op.id_product=p.id
-      INNER JOIN orders as o on op.id_order=o.id";
+          "SELECT SUM(t.sum*p.co2) as total from 
+          (
+            SELECT p.id,SUM(quantity) as sum from orders_products as op 
+            INNER JOIN products as p ON op.id_product=p.id
+            INNER JOIN orders as o on op.id_order=o.id WHERE";
 
         // TODO: handle better the combinations
-        if($product || $country) {
-            $query = $query . ' WHERE';
-        }
         if($product) {
             $query = $query . ' p.id=:product_prop';
         }
@@ -76,15 +94,17 @@ class OrderProductModel extends Model
         if($country) {
             $query = $query . ' o.id=:country_prop';
         }
-
-        if($fromDate && !($product || $country)) {
-            $query = $query . ' WHERE o.date BETWEEN :from_date AND :to_date';
+        if(!($product || $country)) {
+            $query = $query . ' o.date BETWEEN :from_date AND :to_date';
         }
-        if($fromDate && ($product || $country)) {
+        else{
             $query = $query . ' AND o.date BETWEEN :from_date AND :to_date';
         }
-
-        $query = $query . " GROUP BY p.id;";
+        
+        // close the query
+        $query = $query . " GROUP BY p.id
+          ) t 
+          INNER JOIN products as p ON t.id=p.id;";
 
         $st = $this->pdo->prepare($query);
         if($product) {
@@ -93,12 +113,11 @@ class OrderProductModel extends Model
         if($country) {
             $st->bindValue(":country_prop", $country);
         }
-        if($fromDate) {
-            $st->bindValue(":from_date", $fromDate);
-            $st->bindValue(":to_date", $toDate);
-        }
+        $st->bindValue(":from_date", $fromDate);
+        $st->bindValue(":to_date", $toDate);
+
         $st->execute();
 
-        return $st->fetchAll(\PDO::FETCH_ASSOC);
+        return $st->fetch(\PDO::FETCH_ASSOC);
     }
 }
